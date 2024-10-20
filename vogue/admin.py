13cee133d987234ffb41ab6cb05afd0495d4e6c0
maidevalhoon <devalsinghal355@gmail.com>
2024-10-20 20:14:06 +0530

    admin page sortout

--- vogue/admin.py
+++ vogue/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Team, TeamMember
+
+admin.site.register(Team)
+admin.site.register(TeamMember)
+
 
 # Register your models here.
--- vogue/models.py
+++ vogue/models.py
@@ -4,6 +4,9 @@ class Team(models.Model):
     team_name = models.CharField(max_length=100)
     team_leader_name = models.CharField(max_length=100)
     team_leader_email = models.EmailField()
+    
+    def __str__(self):
+        return self.team_name
 
 class TeamMember(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
