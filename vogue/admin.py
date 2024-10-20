f050c762e7ff3c132e79d7d9cacd05c4fea8f855
Mrigank22 <p.mrigank22@gmail.com>
2024-10-20 21:00:18 +0530

    resolved a small mistake in admin.py

--- vogue/admin.py
+++ vogue/admin.py
@@ -4,3 +4,11 @@ from .models import Team, TeamMember
 class TeamMemberInline(admin.TabularInline):
     model = TeamMember
     extra = 1
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('team_name', 'team_leader_name', 'team_leader_email')
+    inlines = [TeamMemberInline]
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ('team', 'member_name', 'member_email')
